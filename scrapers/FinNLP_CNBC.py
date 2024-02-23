dd7fdd2bdee220af6e99c5ab1390a9d16f6e65c2
Jimmy Chen <jc219570512@gmail.com>
2024-02-23 18:33:33 -0500

    Update keyword and added q1 earnings

--- scrapers/FinNLP_CNBC.py
+++ scrapers/FinNLP_CNBC.py
@@ -9,10 +9,10 @@ from finnlp.data_sources.news.cnbc_streaming import CNBC_Streaming
 
 # Download the streaming news data
 news_downloader = CNBC_Streaming()
-news_downloader.download_streaming_search(keyword = "apple stock", rounds = 3)
+news_downloader.download_streaming_search(keyword = "Q1 Earning", rounds = 3)
 
 # Print the first 5 rows of the dataframe
 print(news_downloader.dataframe.head())
 
 # Save the dataframe to a csv file
-news_downloader.dataframe.to_csv(r'C:\Users\jc219\OneDrive\Desktop\GitHub\Data-Centric-FinGPT-Research\scrapers\apple_news.csv', index = False)
+news_downloader.dataframe.to_csv(r'C:\Users\jc219\OneDrive\Desktop\GitHub\Data-Centric-FinGPT-Research\scrapers\q1_earnings.csv', index = False)
