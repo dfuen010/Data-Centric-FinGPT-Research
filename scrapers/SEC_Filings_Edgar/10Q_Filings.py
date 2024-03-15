f8492f135fee0d634800b6bd4bce1c4f572407fe
Ethan <ethan2917@gmail.com>
2024-03-15 14:30:45 -0400

    Update 10-Q Filings scraper to retrieve and analyze Apple Inc. financial data

--- scrapers/SEC_Filings_Edgar/10Q_Filings.py
+++ scrapers/SEC_Filings_Edgar/10Q_Filings.py
@@ -12,20 +12,41 @@ companyData = pd.DataFrame.from_dict(companyTickers.json(), orient="index")
 companyData["cik_str"] = companyData["cik_str"].astype(str).str.zfill(10)
 
 
-cik = companyData.iloc[0]["cik_str"]
-print(cik)
+# Apple Inc.
+cik = companyData.iloc[1]["cik_str"]
+print("Apple Inc. CIK: ", cik)
 
 filingMetadata = requests.get(
     f"https://data.sec.gov/submissions/CIK{cik}.json", headers=headers
 )
-
-# Getting into the filings reports
-print(filingMetadata.json()["filings"]["recent"].keys())
+# All keys for the filings
+# print(filingMetadata.json()["filings"]["recent"].keys())
+# dict_keys(['accessionNumber', 'filingDate', 'reportDate', 'acceptanceDateTime', 'act', 'form', 'fileNumber', 'filmNumber', 'items', 'size', 'isXBRL', 'isInlineXBRL', 'primaryDocument', 'primaryDocDescription'])
 
 allForms = pd.DataFrame.from_dict(filingMetadata.json()["filings"]["recent"])
 
-# Getting the 10-Q filings + variations
-tenQ = allForms[
-    allForms["form"].str.contains("10-Q|10-QT|10QSB|10QSB40|10-Q405|10-Q/A")
-]
+# Getting the 10-Q filings
+tenQ = allForms[allForms["form"].str.contains("10-Q|10Q")]
 print(tenQ.head())
+# Now we can get the 10-Q filings for Apple Inc.
+# and start analyzing the data
+
+for index, row in tenQ.iterrows():
+    accessionNumber = row["accessionNumber"]
+    print(accessionNumber)
+    filing = requests.get(
+        f"https://www.sec.gov/Archives/edgar/data/{cik}/{accessionNumber.replace('-', '')}/{accessionNumber}-index.htm",
+        headers=headers,
+    )
+    print(filing)
+    print(filing.text)
+    break
+
+# The code above will print the accession number and the response from the filing request
+# The response will be a 200 status code, meaning the request was successful
+# The response will contain the HTML of the filing page
+# The HTML will contain the links to the 10-Q filings
+# The links will be used to download the 10-Q filings
+# The 10-Q filings will be used to extract the financial data
+# The financial data will be used to analyze the company's financial health
+# The analysis will be used to make investment decisions
