26d77f05242ce17685c1e5d33912c0fee63c2fb4
Ethan <ethan2917@gmail.com>
2024-03-15 16:30:32 -0400

    Refactor 10Q_Filings.py to extract number of shares from filings

--- scrapers/SEC_Filings_Edgar/10Q_Filings.py
+++ scrapers/SEC_Filings_Edgar/10Q_Filings.py
@@ -1,7 +1,6 @@
 import pandas as pd
 from bs4 import BeautifulSoup
 import requests
-import re
 
 
 headers = {"User-Agent": "email@address.com"}
@@ -34,41 +33,19 @@ tenQ = allForms[allForms["form"].str.contains("10-Q|10Q")]
 # Print all of the 10-Q filings
 print(tenQ)
 
+doc = tenQ.iloc[0]["primaryDocument"]
+print(doc)
 
-# Now we can get the 10-Q filings for Apple Inc.
-# and start analyzing the data
-for index, row in tenQ.iterrows():
-    accessionNumber = row["accessionNumber"]
-    print(accessionNumber)
-    filing = requests.get(
-        f"https://www.sec.gov/Archives/edgar/data/{cik}/{accessionNumber.replace('-', '')}/{accessionNumber}-index.htm",
-        headers=headers,
-    )
-    print(filing)
+accessionNumber = tenQ.iloc[0]["accessionNumber"]
+accessionNumber = accessionNumber.replace("-", "")
+print(accessionNumber)
 
-    # Parse the HTML of the filing
-    soup = BeautifulSoup(filing.content, "html.parser")
+filing = requests.get(
+    f"https://www.sec.gov/Archives/edgar/data/{cik}/{accessionNumber}/{doc}",
+    headers=headers,
+)
 
-    # Define a regular expression pattern to match the tag name
-    tag_pattern = re.compile(r"^ix:nonfraction$")
+parsed_filing = BeautifulSoup(filing.text, "lxml")
 
-    # Find the tags that match the pattern and have the specified attribute
-    shares_tag = soup.find(
-        tag_pattern, attrs={"name": "dei:EntityCommonStockSharesOutstanding"}
-    )
-
-    if shares_tag is not None:
-        num_shares = shares_tag.text.strip()
-        print("Number of shares: ", num_shares)
-    else:
-        print("Shares tag not found")
-
-    break
-# The code above will print the accession number and the response from the filing request
-# The response will be a 200 status code, meaning the request was successful
-# The response will contain the HTML of the filing page
-# The HTML will contain the links to the 10-Q filings
-# The links will be used to download the 10-Q filings
-# The 10-Q filings will be used to extract the financial data
-# The financial data will be used to analyze the company's financial health
-# The analysis will be used to make investment decisions
+shares_tag = parsed_filing.find("ix:nonfraction", {"unitref": "shares"})
+print(shares_tag.text.strip())
