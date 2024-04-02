87ef68d12b84327b7dede5a61c34b5641cb1eeed
Ethan <ethan2917@gmail.com>
2024-04-02 16:58:24 -0400

    Refactor code and add new functionality using XBRL

--- scrapers/SEC_Filings_Edgar/10Q/10Q_Filings.py
+++ scrapers/SEC_Filings_Edgar/10Q/10Q_Filings.py
@@ -1,62 +1,76 @@
 import pandas as pd
 from bs4 import BeautifulSoup
+from xbrl import XBRLParser, GAAP, GAAPSerializer, DEISerializer
 import requests
 
+def main():
+    cik, headers = grab_apple_ticker()
+    ten_q = get_filing_metadata(cik, headers)
+    print(ten_q)
+    parse_filings(ten_q, headers)
 
-headers = {"User-Agent": "email@address.com"}
+    # Get the XBRL data for the 10-Q filing
+    # parse_xbrl_data("0000320193-21-000056")
 
-companyTickers = requests.get(
-    "https://www.sec.gov/files/company_tickers.json", headers=headers
-)
+def grab_apple_ticker():
+    headers = {"User-Agent": "email@address.com"}
 
-companyData = pd.DataFrame.from_dict(companyTickers.json(), orient="index")
+    company_tickers = requests.get(
+        "https://www.sec.gov/files/company_tickers.json", headers=headers
+    )
 
-companyData["cik_str"] = companyData["cik_str"].astype(str).str.zfill(10)
+    company_data = pd.DataFrame.from_dict(company_tickers.json(), orient="index")
+
+    company_data["cik_str"] = company_data["cik_str"].astype(str).str.zfill(10)
 
 
-# Apple Inc.
-cik = companyData.iloc[1]["cik_str"]
-print("Apple Inc. CIK: ", cik)
+    # Apple Inc.
+    cik = company_data.iloc[1]["cik_str"]
+    print("Apple Inc. CIK: ", cik)
+    return cik, headers
 
-filingMetadata = requests.get(
-    f"https://data.sec.gov/submissions/CIK{cik}.json", headers=headers
-)
 # All keys for the filings
 # print(filingMetadata.json()["filings"]["recent"].keys())
 # dict_keys(['accessionNumber', 'filingDate', 'reportDate', 'acceptanceDateTime', 'act', 'form', 'fileNumber', 'filmNumber', 'items', 'size', 'isXBRL', 'isInlineXBRL', 'primaryDocument', 'primaryDocDescription'])
 
-allForms = pd.DataFrame.from_dict(filingMetadata.json()["filings"]["recent"])
+def get_filing_metadata(cik, headers):
+    filing_metadata = requests.get(
+        f"https://data.sec.gov/submissions/CIK{cik}.json", headers=headers
+    )
 
-# Getting the 10-Q filings
-tenQ = allForms[allForms["form"].str.contains("10-Q|10Q")]
-tenQ.to_csv("10Q_filings.csv")
+    all_forms = pd.DataFrame.from_dict(filing_metadata.json()["filings"]["recent"])
 
-# Print all of the 10-Q filings
-#print(tenQ.head())
+    # Getting the 10-Q filings
+    ten_q = all_forms[all_forms["form"].str.contains("10-Q|10Q")]
+    ten_q.to_csv("10Q_apple_filings.csv")
 
-doc = tenQ.iloc[0]["primaryDocument"]
+    return ten_q
 
-for i in range(0, len(tenQ)):
-    acceptanceDateTime = tenQ.iloc[i]["acceptanceDateTime"]
-    acceptanceYear = acceptanceDateTime[:4]
-    accessionNumber = tenQ.iloc[i]["accessionNumber"]
-    accessionNumber = accessionNumber.replace("-", "")
-    print("Document: ", accessionNumber)
+def parse_filings(ten_q, headers):
+    for i in range(0, len(ten_q)):
+        acceptance_date_time = ten_q.iloc[i]["acceptanceDateTime"]
+        acceptance_year = acceptance_date_time[:4]
+        accession_number = ten_q.iloc[i]["accessionNumber"]
+        accession_number = accession_number.replace("-", "")
+        parse_xbrl_data(accession_number, headers)
 
-    filing = requests.get(
-        f"https://www.sec.gov/Archives/edgar/data/{cik}/{accessionNumber}/{doc}",
+# Getting the XBRL data for the 10-Q filing based on the accession number
+def parse_xbrl_data(accession_number, headers):
+    xbrl_filing = requests.get(
+        f"https://www.sec.gov/Archives/edgar/data/{cik}/{accession_number}/{doc}",
         headers=headers,
     )
 
-    parsed_filing = BeautifulSoup(filing.text, "lxml")
+    xbrl_soup = BeautifulSoup(xbrl_filing.text, features="xml")
 
-    all_ix = parsed_filing.find_all("ix:nonfraction")
+    xbrl_parser = XBRLParser()
 
-    shares_tag = parsed_filing.find("ix:nonfraction", attrs={"unitref": "shares"})
+    xbrl = xbrl_parser.parse(xbrl_soup)
 
-    print(
-        "Shares of common stock were issued and outstanding in",
-        acceptanceYear,
-        ": ",
-        shares_tag.text.strip(),
-    )
+    gaap_obj = xbrl_parser.parseGAAP(xbrl, doc)
+
+    gaap_serializer = GAAPSerializer()
+
+    gaap_data = gaap_serializer.serialize(gaap_obj)
+
+    return gaap_data
