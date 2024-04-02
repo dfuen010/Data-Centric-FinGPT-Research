8aa9b8ec593acad74aeffda93f4d8dd98c5673cf
Ethan <ethan2917@gmail.com>
2024-04-02 17:17:44 -0400

    Refactor 10Q_Filings.py script and update CSV file path

--- scrapers/SEC_Filings_Edgar/10Q/10Q_Filings.py
+++ scrapers/SEC_Filings_Edgar/10Q/10Q_Filings.py
@@ -3,15 +3,7 @@ from bs4 import BeautifulSoup
 from xbrl import XBRLParser, GAAP, GAAPSerializer, DEISerializer
 import requests
 
-def main():
-    cik, headers = grab_apple_ticker()
-    ten_q = get_filing_metadata(cik, headers)
-    print(ten_q)
-    parse_filings(ten_q, headers)
-
-    # Get the XBRL data for the 10-Q filing
-    # parse_xbrl_data("0000320193-21-000056")
-
+# Grabs the CIK for Apple Inc. from the SEC
 def grab_apple_ticker():
     headers = {"User-Agent": "email@address.com"}
 
@@ -29,11 +21,11 @@ def grab_apple_ticker():
     print("Apple Inc. CIK: ", cik)
     return cik, headers
 
-# All keys for the filings
-# print(filingMetadata.json()["filings"]["recent"].keys())
-# dict_keys(['accessionNumber', 'filingDate', 'reportDate', 'acceptanceDateTime', 'act', 'form', 'fileNumber', 'filmNumber', 'items', 'size', 'isXBRL', 'isInlineXBRL', 'primaryDocument', 'primaryDocDescription'])
-
+# Grabs the filing metadata for the company from the SEC
 def get_filing_metadata(cik, headers):
+    # All keys for the filings:
+    # dict_keys(['accessionNumber', 'filingDate', 'reportDate', 'acceptanceDateTime', 'act', 'form', 'fileNumber',
+    # 'filmNumber', 'items', 'size', 'isXBRL', 'isInlineXBRL', 'primaryDocument', 'primaryDocDescription'])
     filing_metadata = requests.get(
         f"https://data.sec.gov/submissions/CIK{cik}.json", headers=headers
     )
@@ -42,20 +34,22 @@ def get_filing_metadata(cik, headers):
 
     # Getting the 10-Q filings
     ten_q = all_forms[all_forms["form"].str.contains("10-Q|10Q")]
-    ten_q.to_csv("10Q_apple_filings.csv")
+    # Adding all the 10-Q filings to a CSV file
+    # Save this CSV file to the current folder where the script is running
+    ten_q.to_csv("./scrapers/SEC_Filings_Edgar/10Q/10Q_apple_filings.csv", index=False)
 
-    return ten_q
+    doc = ten_q.iloc[0]["primaryDocument"]
+    return ten_q, doc
 
-def parse_filings(ten_q, headers):
+# Parsing the 10-Q filings
+def parse_filings(ten_q, headers, doc, cik):
     for i in range(0, len(ten_q)):
-        acceptance_date_time = ten_q.iloc[i]["acceptanceDateTime"]
-        acceptance_year = acceptance_date_time[:4]
         accession_number = ten_q.iloc[i]["accessionNumber"]
         accession_number = accession_number.replace("-", "")
-        parse_xbrl_data(accession_number, headers)
+        parse_xbrl_data(accession_number, headers, doc, cik)
 
 # Getting the XBRL data for the 10-Q filing based on the accession number
-def parse_xbrl_data(accession_number, headers):
+def parse_xbrl_data(accession_number, headers, doc, cik):
     xbrl_filing = requests.get(
         f"https://www.sec.gov/Archives/edgar/data/{cik}/{accession_number}/{doc}",
         headers=headers,
@@ -74,3 +68,11 @@ def parse_xbrl_data(accession_number, headers):
     gaap_data = gaap_serializer.serialize(gaap_obj)
 
     return gaap_data
+
+# Run the script
+if __name__ == "__main__":
+    print("Running the 10-Q Filings script")
+    cik, headers = grab_apple_ticker()
+    ten_q, doc = get_filing_metadata(cik, headers)
+    print(ten_q)
+    parse_filings(ten_q, headers, doc, cik)
