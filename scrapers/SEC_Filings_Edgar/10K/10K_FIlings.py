69dcd01a3e5dda3f5c84978b4ff5c266e5821480
c-jimmy1 <74884278+c-jimmy1@users.noreply.github.com>
2024-04-02 20:17:53 -0400

    created new function to get each section's information

--- scrapers/SEC_Filings_Edgar/10K/10K_FIlings.py
+++ scrapers/SEC_Filings_Edgar/10K/10K_FIlings.py
@@ -38,7 +38,6 @@ def get_filing_form(headers, cik):
     # Getting the 10-K filings + variations
 
     all_10k_forms = all_forms[all_forms['form'].str.contains('10-K|10-KT|10KSB|10KT405|10KSB40|10-K405|10-K/A')]
-    all_10k_forms.to_csv('10k_microsoft_files.csv', index=False)
     # example for 1st form
     first_form = all_10k_forms.iloc[0]["primaryDocument"]
 
@@ -47,14 +46,15 @@ def get_filing_form(headers, cik):
     return f"https://www.sec.gov/Archives/edgar/data/{cik}/{accession_number}/{first_form}"
     
     
-def extract_page_directory(filing_link):
+
+"""
+This function will extract the page's table of contents from the filing link given
+    args: soup
+    returns: df
+"""
+def extract_page_directory(soup):
     # Getting the XBRL data
-    filing_data = requests.get(filing_link, headers=headers)
     
-    soup = BeautifulSoup(filing_data.text, features="xml")
-    # with open('table_of_contents.txt', 'w', encoding='utf-8') as file:
-    #     file.write(soup.prettify()) 
-   
     specific_span = soup.find_all('span', style='background-color:rgba(0,0,0,0);color:rgba(0,0,0,1);white-space:pre-wrap;font-weight:normal;font-size:10.0pt;font-family:"Arial", sans-serif;min-width:fit-content;')
 
     df = pd.DataFrame(columns=['Item Number', 'Topic', 'Page Number'])
@@ -62,10 +62,11 @@ def extract_page_directory(filing_link):
     for i in range(len(specific_span)):
         if "Item" in specific_span[i].text:
             df.loc[len(df)] = [str(specific_span[i].text), None, None]
-        elif specific_span[i].text == "[Reserved]":
-            continue
         elif (specific_span[i].text).isdigit():
-            df.loc[len(df)-1, 'Page Number'] = specific_span[i].text
+            if df.loc[len(df)-1, 'Page Number'] is None:
+                df.loc[len(df)-1, 'Page Number'] = [specific_span[i].text]
+            else:
+                df.loc[len(df)-1, 'Page Number'].append(specific_span[i].text)
         else:
             if df.loc[len(df)-1, 'Topic'] is None:
                 df.loc[len(df)-1, 'Topic'] = [specific_span[i].text]
@@ -75,7 +76,33 @@ def extract_page_directory(filing_link):
             break            
 
     return df
+
+"""
+This function will extract the sections from the page's table of contents
+    args: df (table of contents)
+    returns: df (Data from each section)
+"""
+def extract_sections(table_contents, soup):
+
+    # with open('table_of_contents.txt', 'w', encoding='utf-8') as file:
+    #     file.write(soup.prettify()) 
     
+    span_texts = soup.find_all('span', style='background-color:rgba(0,0,0,0);color:rgba(0,0,0,1);white-space:pre-wrap;font-weight:normal;font-size:10.0pt;font-family:"Arial", sans-serif;min-width:fit-content;')
+
+    for i in range(len(table_contents)):
+        section_titles = table_contents.loc[i, 'Topic']
+        page_numbers = table_contents.loc[i, 'Page Number']
+        item_number = table_contents.loc[i, 'Item Number']
+        for j in range(len(section_titles)):
+            title = section_titles[j]
+            page_number = page_numbers[j]
+            item_with_title_abbr = item_number + " " + title[0]
+            for span in span_texts:
+                if span.text == item_with_title_abbr:
+                    print(span.text)
+        # break at first to prevent long run time
+        if i == 1:
+            break
 
 if __name__ == "__main__":
     headers = {'User-Agent': "email@address.com"}
@@ -83,7 +110,21 @@ if __name__ == "__main__":
     companyData = get_tickers(headers)        
 
     filing_link = get_filing_form(headers, companyData.iloc[0]['cik_str'])
+    print(filing_link)
+
+    filing_data = requests.get(filing_link, headers=headers)
     
-    df = extract_page_directory(filing_link)
+    soup = BeautifulSoup(filing_data.text, features="xml")
     
-    df.to_csv('10k_microsoft.csv', index=False)
+    table_contents = extract_page_directory(soup)
+
+    extract_sections(table_contents, soup)
+
+
+    # file_test = r"10k_microsoft.csv"
+    # os.chdir(r"C:\Users\Jimmy\OneDrive\Desktop\GitHub\Data-Centric-FinGPT-Research\scrapers\SEC_Filings_Edgar\10K")
+    # print(os.getcwd())
+    # new_df = pd.read_csv(file_test)
+    # print(new_df)
+
+    
