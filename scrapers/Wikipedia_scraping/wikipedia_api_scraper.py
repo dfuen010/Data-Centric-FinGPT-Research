50203b5ed5153da5451250786ed8bdaed7d6945f
dfuen010 <dfuen010@gmail.com>
2024-04-12 17:23:54 -0400

    Reformatting how wikipedia output is being written to file

--- scrapers/Wikipedia_scraping/wikipedia_api_scraper.py
+++ scrapers/Wikipedia_scraping/wikipedia_api_scraper.py
@@ -17,6 +17,12 @@ if __name__ == "__main__":
         #for title in sorted(links.keys()):
         #    print("%s: %s" % (title, links[title]))
 
+    def write_by_sections(sections, output_file, level=0):
+        for s in sections:
+            output_file.write("{}: {} \n\n".format("*" * (level + 1), s.title))
+            encoded_text = " ".join(re.split("[^A-Za-z0-9]+", s.text)) 
+            output_file.write("{} \n\n".format(encoded_text))
+            write_by_sections(s.sections, output_file, level + 1)
 
     # create a wikipedia object using the API with our user agent
     # this wikipedia object initalization creates our request to the wikipedia API using the User-Agent as the header
@@ -28,8 +34,11 @@ if __name__ == "__main__":
     )
 
     # specifies a page to scrape text from 
-    page_text = wiki_wiki.page('Python_(programming_language)').text
+    current_page = wiki_wiki.page('Python_(programming_language)')
+    page_text = current_page.text
+    output_file.write("Page Title - " + current_page.title + "\n\n")
+    # remove any non-ascii values that may appear from the page text 
     encoded_text = " ".join(re.split("[^A-Za-z0-9]+", page_text)) 
-    output_file.write(encoded_text)
+    write_by_sections(current_page.sections, output_file)
 
     output_file.close()
