d74a6d77d6d377b7c6899a0d15d0939575ea2891
Ethan <ethan2917@gmail.com>
2024-04-12 11:12:18 -0400

    Refactored CNBC scraper to retrieve earnings chart data and export it to a JSON file

--- scrapers/cnbc/cnbc_scraper.py
+++ scrapers/cnbc/cnbc_scraper.py
@@ -1,14 +1,37 @@
 import requests
 
-url = "https://cnbc.p.rapidapi.com/symbols/get-earnings-chart"
-
-querystring = {"issueId":"36276","numberOfYears":"3"}
+url = "https://cnbc.p.rapidapi.com/symbols/translate"
 
 headers = {
 	"X-RapidAPI-Key": "4d6d8e080bmsh68dcf84f504154ap1335bbjsn0a70ea825650",
 	"X-RapidAPI-Host": "cnbc.p.rapidapi.com"
 }
 
+querystring = {"symbol":"TSLA"}
+
+tslaID = requests.get(url, headers=headers, params=querystring)
+
+querystring = {"symbol":"AAPL"}
+
+aaplID = requests.get(url, headers=headers, params=querystring)
+
+querystring = {"symbol":"MSFT"}
+msftID = requests.get(url, headers=headers, params=querystring)
+
+print(tslaID.json())
+print(aaplID.json())
+print(msftID.json())
+
+url = "https://cnbc.p.rapidapi.com/symbols/v2/get-chart"
+querystring = {"symbol":"TSLA","interval":"1M"}
 response = requests.get(url, headers=headers, params=querystring)
 
+url = "https://cnbc.p.rapidapi.com/symbols/get-summary"
+querystring = {"issueIds":"36276,24812378"}
+response = requests.get(url, headers=headers, params=querystring)
 print(response.json())
+
+
+# Export the data to a json file
+with open("tsla_chart.json", "w") as f:
+    f.write(response.text)
