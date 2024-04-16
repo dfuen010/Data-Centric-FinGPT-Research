e7283485bdfaa3e7e2ddf4c2581d20f5a6f30b2e
dfuen010 <dfuen010@gmail.com>
2024-04-16 17:21:02 -0400

    addressed case of reaching a dead-end (page with no links) to continue scraping

--- scrapers/Wikipedia_scraping/wikipedia_api_scraper.py
+++ scrapers/Wikipedia_scraping/wikipedia_api_scraper.py
@@ -12,6 +12,7 @@ output_file = open("wikipedia_data.txt", "w")
 if __name__ == "__main__":
     def write_by_sections(sections, output_file, level=0):
         for s in sections:
+            encoded_title = " ".join(re.split("[^A-Za-z0-9]+", s.title)) 
             output_file.write("{}: {} \n\n".format("*" * (level + 1), s.title))
             encoded_text = " ".join(re.split("[^A-Za-z0-9]+", s.text)) 
             output_file.write("{} \n\n".format(encoded_text))
@@ -26,17 +27,21 @@ if __name__ == "__main__":
             extract_format=wikipediaapi.ExtractFormat.WIKI
     )
 
-    # We will scrape 100 pages from wikipedia to start with this scraper starting with the wiki page for python
-    pages = 100
+    # We will scrape 200 pages from wikipedia to start with this scraper starting with the wiki page for python
+    pages = 200
     i = 0
-    page_string = 'Python_(programming_language)'
+    page_string = "Bank"
     # specifies a page to scrape text from 
     current_page = wiki_wiki.page(page_string)
+
+    # let's have an array that keeps track of the pages we have visited in the case we go to dead end (page with no link)
+    visited_pages = []
     while i != pages:
 
         i += 1
         page_text = current_page.text
-        output_file.write("Page Title - " + current_page.title + "\n\n")
+        encoded_title = " ".join(re.split("[^A-Za-z0-9]+", current_page.title)) 
+        output_file.write("Page Title - " + encoded_title + "\n\n")
         # remove any non-ascii values that may appear from the page text 
         encoded_text = " ".join(re.split("[^A-Za-z0-9]+", page_text)) 
         write_by_sections(current_page.sections, output_file)
@@ -47,11 +52,15 @@ if __name__ == "__main__":
         if len(links) != 0:
             link_to_visit = random.choice(list(links.keys()))
             current_page = links[link_to_visit]
+            visited_pages.append(current_page)
             output_file.write("\n\n")
         else:
-            # if there are no external links we'll stop for now
-            break
-        
+            # if there are any pages on our stack and we go to the previous and select a different link
+            if visited_pages:
+                current_page = visited_pages.pop()
+            else:
+                # we have no more pages to go back to so we break out
+                break
         
 
     output_file.close()
