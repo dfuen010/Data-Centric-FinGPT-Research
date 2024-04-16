563a36a7977ee49f792501b3ee87f88a9e6e8ed8
dfuen010 <dfuen010@gmail.com>
2024-04-16 16:49:41 -0400

    Scraper now pulls data from multiple wikipedia pages

--- scrapers/Wikipedia_scraping/wikipedia_api_scraper.py
+++ scrapers/Wikipedia_scraping/wikipedia_api_scraper.py
@@ -1,7 +1,7 @@
 import wikipediaapi
 import requests
 import re
-
+import random
 import requests
 
 # file for writing the text data we obtain from wikipedia
@@ -10,13 +10,6 @@ output_file = open("wikipedia_data.txt", "w")
 
 # main code for scraping wikipedia 
 if __name__ == "__main__":
-
-    def get_links(page):
-        links = page.links
-        print(links)
-        #for title in sorted(links.keys()):
-        #    print("%s: %s" % (title, links[title]))
-
     def write_by_sections(sections, output_file, level=0):
         for s in sections:
             output_file.write("{}: {} \n\n".format("*" * (level + 1), s.title))
@@ -33,12 +26,32 @@ if __name__ == "__main__":
             extract_format=wikipediaapi.ExtractFormat.WIKI
     )
 
+    # We will scrape 100 pages from wikipedia to start with this scraper starting with the wiki page for python
+    pages = 100
+    i = 0
+    page_string = 'Python_(programming_language)'
     # specifies a page to scrape text from 
-    current_page = wiki_wiki.page('Python_(programming_language)')
-    page_text = current_page.text
-    output_file.write("Page Title - " + current_page.title + "\n\n")
-    # remove any non-ascii values that may appear from the page text 
-    encoded_text = " ".join(re.split("[^A-Za-z0-9]+", page_text)) 
-    write_by_sections(current_page.sections, output_file)
+    current_page = wiki_wiki.page(page_string)
+    while i != pages:
+
+        i += 1
+        page_text = current_page.text
+        output_file.write("Page Title - " + current_page.title + "\n\n")
+        # remove any non-ascii values that may appear from the page text 
+        encoded_text = " ".join(re.split("[^A-Za-z0-9]+", page_text)) 
+        write_by_sections(current_page.sections, output_file)
+
+        # next let's get the links to next pages and go to the next page and continue this loop
+        links = current_page.links
+        # let's go to a random page in our list of links
+        if len(links) != 0:
+            link_to_visit = random.choice(list(links.keys()))
+            current_page = links[link_to_visit]
+            output_file.write("\n\n")
+        else:
+            # if there are no external links we'll stop for now
+            break
+        
+        
 
     output_file.close()
