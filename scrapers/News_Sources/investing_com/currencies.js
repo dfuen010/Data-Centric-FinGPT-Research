f1aa4b9930ba290e4dce76339d9c3db0393ce9ea
j-sund <joshua.sundararaman@gmail.com>
2024-04-24 01:12:58 -0400

    Added Another market of US Dollars

--- scrapers/News_Sources/investing_com/currencies.js
+++ scrapers/News_Sources/investing_com/currencies.js
@@ -9,14 +9,18 @@ async function main() {
     const currencyEUROtoUSD = await investing("1"); 
     // id 2 stands for british pounnd us currency comparison
     const currencyPOUNDtoUSD = await investing("2"); 
+    // id 8827 for USD Index Futures
+    const usDollarFutures = await investing("8827");
 
     // Convert responses to JSON with indentation
-    const jsonContent = JSON.stringify({ currencyEUROtoUSD, currencyPOUNDtoUSD }, null, 4);
+    const jsonContent = JSON.stringify({ currencyEUROtoUSD, currencyPOUNDtoUSD, usDollarFutures }, null, 4);
+
+    
 
     // Write formatted JSON to a file
     await fs.writeFile('currency.txt', jsonContent);
 
-    console.log('Responses written to responses.txt file.');
+    console.log('Responses written to currency.txt file.');
 
   } catch (err) {
     console.error(err);
