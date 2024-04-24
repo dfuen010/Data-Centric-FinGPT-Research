42bd6b25aba3e91185507a6caf4ce91427ecfb9e
j-sund <joshua.sundararaman@gmail.com>
2024-04-23 22:30:37 -0400

    Added the Final DataSet for Top 50 Companies

--- scrapers/yahoo_finance/top50scraper.py
+++ scrapers/yahoo_finance/top50scraper.py
@@ -1,28 +1,36 @@
 import yfinance as yf
 import json
 
+# Get Directory Path
 file_path = "scrapers/yahoo_finance/top50.txt"
-
 listCompanies = []
 
+# Open Company Text File
 with open(file_path, "r") as f:
     
-    # go thru text file
+    # Go Through Each Line (each line is company ticker)
     for line in f:
         
-        #add companies to array
+        #add companies to list
         company = line.strip()
         listCompanies.append(company)
 
+# Open file to write Company Financials
 with open("datasets/news/yahoo_finance/top50financials.txt", "w") as f:
     
+    # Go through previously created List
     for company in listCompanies:
         
+        # Get Ticker
         ticker = yf.Ticker(company)
+        # Get Income and Balance Statements
         incomeStatement = ticker.income_stmt
         balanceSheet = ticker.balance_sheet
+        # Check if there is there income/balanace info available
         if(balanceSheet.columns.empty):
+            # if not, go to next company
             continue
+        # Format and Write to File the Required Information
         f.write(f"\n\n{company} Financials(Income Statement/Balance Sheet):\n\n")
         f.write(f"Income Statement for {company}{incomeStatement}\n\n")
         f.write(f"Balance Sheet for {company}{balanceSheet.to_string()}\n\n")
